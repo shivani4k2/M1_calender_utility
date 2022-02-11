ae5a7ea7c93587bec057991ca5ee29e1d39e1cef
shivani4k2 <85895650+shivani4k2@users.noreply.github.com>
2022-02-11 13:29:07 +0530

    Add files via upload

---
+++ 3_Implementation/header/project.h
@@ -0,0 +1,61 @@
+/**
+ * @file project.h
+ * @author devabhaktuni shivani
+ * @brief 
+ * @version 0.1
+ * @date 2022-02-11
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#ifndef PROJECT_H_
+#define PROJECT_H_
+
+#include<stdio.h>
+#include<conio.h>
+#include<windows.h>
+
+
+/**
+ * @brief 
+ * 
+ * @param a 
+ * @param b 
+ * @param c 
+ * @return int 
+ */
+int date(int a, int b, int c);
+
+
+
+/**
+ * @brief 
+ * 
+ * @param tot 
+ * @return char* 
+ */
+char* getmpe(int tot);
+
+
+
+/**
+ * @brief 
+ * 
+ * @param m 
+ * @param n 
+ * @return int 
+ */
+int dayscal(int m, int n);
+
+
+
+/**
+ * @brief 
+ * 
+ * @param in 
+ */
+void calp(int in);
+
+
+
+#endif
